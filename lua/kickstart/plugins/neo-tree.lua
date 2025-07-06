ef04b76c5f0de8db14b93ab99f09843d9bbf7f24
Dhairya Desai <me@desaidn.dev>
2025-07-05 23:54:14 -0400

    Enhance neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,42 +11,75 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+    { '<leader>e', ':Neotree toggle<CR>', desc = 'Toggle [E]xplorer', silent = true },
   },
   opts = {
     window = {
-      position = "right",
-      width = 30,
+      position = 'right',
+      width = 40,
     },
     default_component_configs = {
       icon = {
-        folder_closed = "[+]",
-        folder_open = "[-]",
-        folder_empty = "[.]",
-        default = "",
+        folder_closed = '[+]',
+        folder_open = '[-]',
+        folder_empty = '[.]',
+        default = '',
       },
       git_status = {
         symbols = {
-          added = "+",
-          modified = "~",
-          deleted = "-",
-          renamed = "r",
-          untracked = "?",
-          ignored = "!",
-          unstaged = "u",
-          staged = "s",
-          conflict = "c",
-        }
-      }
+          added = '+',
+          modified = '~',
+          deleted = '-',
+          renamed = 'r',
+          untracked = '?',
+          ignored = '!',
+          unstaged = 'u',
+          staged = 's',
+          conflict = 'c',
+        },
+      },
     },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
         hide_hidden = false,
+        hide_gitignored = false,
+        hide_by_name = {},
+        hide_by_pattern = {},
+        always_show = {},
+        never_show = {},
       },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
+      use_libuv_file_watcher = true,
       window = {
-        mappings = {
-        },
+        mappings = {},
+      },
+    },
+    event_handlers = {
+      {
+        event = 'file_opened',
+        handler = function(file_path)
+          vim.schedule(function()
+            local neo_tree_wins = vim.tbl_filter(function(win)
+              return vim.bo[vim.api.nvim_win_get_buf(win)].filetype == 'neo-tree'
+            end, vim.api.nvim_list_wins())
+
+            if #neo_tree_wins > 0 then
+              vim.api.nvim_set_current_win(neo_tree_wins[1])
+            end
+          end)
+        end,
+      },
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          vim.wo.number = false
+          vim.wo.relativenumber = false
+          vim.wo.signcolumn = 'no'
+        end,
       },
     },
   },
