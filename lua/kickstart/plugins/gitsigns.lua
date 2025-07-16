9c2b13a7bf19685e848e03e8c2be58eee781e3f5
Dhairya Desai <me@desaidn.dev>
2025-07-15 22:43:17 -0400

    Revert "Improve editor speed"
    This reverts commit 12ae1e41f0095c2a26f3d48b5f7f80767a9cf6d7.

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -19,7 +19,7 @@ return {
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = 'eol', -- end of line
-        delay = 50, -- minimal delay for instant blame
+        delay = 200, -- slight delay before showing
         ignore_whitespace = false,
       },
       on_attach = function(bufnr)
