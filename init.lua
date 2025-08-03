9c8f6947b535dbb897570c0aa6ac62b778612e72
Dhairya Desai <me@desaidn.dev>
2025-08-02 20:54:49 -0400

    Update treesitter context opts

--- init.lua
+++ init.lua
@@ -1354,13 +1354,44 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter-context',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     opts = {
+      -- Enable this plugin (Can be enabled/disabled later via commands)
       enable = true,
-      max_lines = 20,
-      min_window_height = 20,
+
+      -- Enable multiwindow support.
+      multiwindow = false,
+
+      -- How many lines the window should span. Values <= 0 mean no limit.
+      -- Can be '<int>%' like '30%' - to specify percentage of win.height
+      max_lines = 0,
+
+      -- Minimum editor window height to enable context. Values <= 0 mean no
+      -- limit.
+      min_window_height = 0,
+
+      -- Whether to show line numbers
       line_numbers = true,
-      multiline_threshold = 1,
+
+      -- Maximum number of lines to show for a single context
+      multiline_threshold = 20,
+
+      -- Which context lines to discard if `max_lines` is exceeded.
+      -- Choices: 'inner', 'outer'
       trim_scope = 'outer',
+
+      -- Line used to calculate context.
+      -- Choices: 'cursor', 'topline'
       mode = 'cursor',
+
+      -- Separator between context and content. Should be a single character
+      -- string, like '-'. When separator is set, the context will only show
+      -- up when there are at least 2 lines above cursorline.
+      separator = nil,
+
+      -- The Z-index of the context window
+      zindex = 20,
+
+      -- (fun(buf: integer): boolean) return false to disable attaching
+      on_attach = nil,
     },
   },
 
