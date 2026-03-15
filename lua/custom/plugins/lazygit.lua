1471d04de4ee30fe63500140a0e7114f76d8e97c
Dhairya Desai <me@desaidn.dev>
2026-03-14 20:40:47 -0400

    Simplify lazygit by using a floating window

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,40 +1,36 @@
---- Opens lazygit in a dedicated tab. If already open, focuses the existing tab.
---- Quitting lazygit (q) automatically closes the tab and returns to the previous buffer.
+--- Opens lazygit in a fullscreen floating window.
+--- Quitting lazygit (q) automatically closes the float and returns to the previous buffer.
 --- Requires: lazygit (https://github.com/jesseduffield/lazygit)
-local lazygit_buf = nil
+local function open_lazygit()
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width = vim.o.columns
+  local height = vim.o.lines
 
-local function toggle_lazygit()
-  if lazygit_buf and vim.api.nvim_buf_is_valid(lazygit_buf) then
-    for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-      for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
-        if vim.api.nvim_win_get_buf(win) == lazygit_buf then
-          vim.api.nvim_set_current_tabpage(tab)
-          vim.api.nvim_set_current_win(win)
-          vim.api.nvim_command('startinsert')
-          return
-        end
-      end
-    end
-  end
+  vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height - 2,
+    row = 0,
+    col = 0,
+    style = 'minimal',
+    border = 'none',
+  })
 
-  vim.api.nvim_command('tabnew')
-  lazygit_buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_set_option_value('winhl', 'NormalFloat:Normal', { win = vim.api.nvim_get_current_win() })
+
   vim.fn.jobstart('lazygit', {
     term = true,
     on_exit = function(_, _, _)
-      if lazygit_buf and vim.api.nvim_buf_is_valid(lazygit_buf) then
-        vim.api.nvim_buf_delete(lazygit_buf, { force = true })
-      end
-      lazygit_buf = nil
+      if buf and vim.api.nvim_buf_is_valid(buf) then vim.api.nvim_buf_delete(buf, { force = true }) end
     end,
   })
-  vim.api.nvim_command('startinsert')
+  vim.cmd.startinsert()
 end
 
 return {
   'lazygit',
   virtual = true,
   keys = {
-    { '<leader>gg', toggle_lazygit, desc = 'Lazygit' },
+    { '<leader>gg', open_lazygit, desc = 'Lazygit' },
   },
 }
