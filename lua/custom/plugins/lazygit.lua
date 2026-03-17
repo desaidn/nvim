8c91dd24e535aef3eb850391c6535845b5ade995
Dhairya Desai <me@desaidn.dev>
2026-03-17 08:39:47 -0400

    Replace lazygit float with toggleable floating window

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,29 +1,51 @@
---- Opens lazygit in a fullscreen floating window.
---- Quitting lazygit (q) automatically closes the float and returns to the previous buffer.
+--- Toggleable lazygit in a fullscreen floating window.
+--- <leader>g opens lazygit, <leader>g again hides it (preserving state).
+--- Quitting lazygit (q) fully cleans up the buffer and window.
 --- Requires: lazygit (https://github.com/jesseduffield/lazygit)
-local function open_lazygit()
-  local buf = vim.api.nvim_create_buf(false, true)
-  local width = vim.o.columns
-  local height = vim.o.lines
+local state = { buf = nil, win = nil }
 
-  vim.api.nvim_open_win(buf, true, {
+local function float_opts()
+  return {
     relative = 'editor',
-    width = width,
-    height = height - 2,
+    width = vim.o.columns,
+    height = vim.o.lines - 2,
     row = 0,
     col = 0,
     style = 'minimal',
     border = 'none',
-  })
+  }
+end
 
-  vim.api.nvim_set_option_value('winhl', 'NormalFloat:Normal', { win = vim.api.nvim_get_current_win() })
+local function toggle_lazygit()
+  if state.win and vim.api.nvim_win_is_valid(state.win) then
+    vim.api.nvim_win_hide(state.win)
+    state.win = nil
+    return
+  end
+
+  if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
+    state.win = vim.api.nvim_open_win(state.buf, true, float_opts())
+    vim.api.nvim_set_option_value('winhl', 'NormalFloat:Normal', { win = state.win })
+    vim.cmd.startinsert()
+    return
+  end
+
+  state.buf = vim.api.nvim_create_buf(false, true)
+  state.win = vim.api.nvim_open_win(state.buf, true, float_opts())
+  vim.api.nvim_set_option_value('winhl', 'NormalFloat:Normal', { win = state.win })
 
   vim.fn.jobstart('lazygit', {
     term = true,
-    on_exit = function(_, _, _)
-      if buf and vim.api.nvim_buf_is_valid(buf) then vim.api.nvim_buf_delete(buf, { force = true }) end
+    on_exit = function()
+      if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
+        vim.api.nvim_buf_delete(state.buf, { force = true })
+      end
+      state.buf = nil
+      state.win = nil
     end,
   })
+
+  vim.keymap.set('t', '<leader>g', toggle_lazygit, { buffer = state.buf, desc = 'Toggle Lazygit' })
   vim.cmd.startinsert()
 end
 
@@ -31,6 +53,6 @@ return {
   'lazygit',
   virtual = true,
   keys = {
-    { '<leader>gg', open_lazygit, desc = 'Lazygit' },
+    { '<leader>g', toggle_lazygit, desc = 'Lazygit' },
   },
 }
